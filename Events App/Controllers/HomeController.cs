b74a006da52679d9098bf69301e9ba960e1a188d
mihaela <mihaela.popescu360@gmail.com>
2023-04-18 13:25:59 +0300

    comentarii

--- Events App/Controllers/HomeController.cs
+++ Events App/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace Events_App.Controllers
 {
+    //comentariiii
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
